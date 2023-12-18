d0197b364ba96519154adcd9b9b995081d727f59
DJ <123333887+JSDWRLD@users.noreply.github.com>
2023-12-18 11:21:40 -0800

    Encryption Machine
    This program replicates caesars cipher which is a kind of substitution cipher.

---
+++ EncryptionMachineMain.java
@@ -0,0 +1,10 @@
+/* 
+   10/26/23
+   Assignment 3: This program replicates caesars cipher which is a kind of substitution cipher.
+*/
+
+public class EncryptionMachineMain {
+   public static void main (String args []){
+      EncryptionMachineMainHelper.runEncrypter();
+   }  
+} 
