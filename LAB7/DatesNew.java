4ab9e599c681650eab66246d8a45fb0ffc2c4134
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-01-02 18:00:21 -0800

    Add files via upload

---
+++ LAB7/DatesNew.java
@@ -0,0 +1,10 @@
+/* 
+   10/13/23
+   LAB 7: This program checks to see if a date that is entered is a valid date.
+*/
+
+public class DatesNew {
+   public static void main (String args []){
+      DatesHelperNew.checkDate();
+   }  
+} 
